96ba8a6a694f8e5176e60f1bcd05497caca681a9
ardilabayufirdaus <ardilabayufirdaus@gmail.com>
2025-10-24 11:39:13 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/SIPOMA-PocketBase/', 
   resolve: {
     alias: {
       '@': '/src',
